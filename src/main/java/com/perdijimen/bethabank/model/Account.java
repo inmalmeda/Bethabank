16c2f757f36f9c3b8f06f950bcadf8c209eed398
mperdiguero <perdi666@gmail.com>
2021-05-11 17:15:00 +0200

    Created account class

---
+++ src/main/java/com/perdijimen/bethabank/model/Account.java
@@ -0,0 +1,97 @@
+package com.perdijimen.bethabank.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String IBAN;
+
+    private Double balance;
+
+    private LocalDate created_at;
+
+    private LocalDate utdated_at;
+
+    public Account() {
+    }
+
+    public Account(String name, String IBAN, Double balance, LocalDate created_at, LocalDate utdated_at) {
+        this.name = name;
+        this.IBAN = IBAN;
+        this.balance = balance;
+        this.created_at = created_at;
+        this.utdated_at = utdated_at;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIBAN() {
+        return IBAN;
+    }
+
+    public void setIBAN(String IBAN) {
+        this.IBAN = IBAN;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public LocalDate getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDate created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDate getUtdated_at() {
+        return utdated_at;
+    }
+
+    public void setUtdated_at(LocalDate utdated_at) {
+        this.utdated_at = utdated_at;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", IBAN='" + IBAN + '\'' +
+                ", balance=" + balance +
+                ", created_at=" + created_at +
+                ", utdated_at=" + utdated_at +
+                '}';
+    }
+}
