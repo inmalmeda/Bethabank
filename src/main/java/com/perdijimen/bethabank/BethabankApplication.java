a4831d832f599b66ade3c5cda1855c49df028256
Inma Jimenez <inmalmeda@hotmail.com>
2021-05-11 16:05:53 +0200

    First commit

---
+++ src/main/java/com/perdijimen/bethabank/BethabankApplication.java
@@ -0,0 +1,13 @@
+package com.perdijimen.bethabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BethabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BethabankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perdijimen/bethabank/BethabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perdijimen.bethabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BethabankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
