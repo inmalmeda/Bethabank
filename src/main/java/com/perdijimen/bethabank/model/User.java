62631252feedda26bbc7448d314291351d115173
mperdiguero <perdi666@gmail.com>
2021-05-11 16:59:18 +0200

    first commit develop

--- src/main/java/com/perdijimen/bethabank/model/User.java
+++ src/main/java/com/perdijimen/bethabank/model/User.java
@@ -1,9 +1,8 @@
 package com.perdijimen.bethabank.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
 
 @Entity
 @Table(name = "users")
@@ -11,6 +10,47 @@ public class User {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String lastname;
+
+    @Column
+    private Date birth_day;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String DNI;
+
+    @Column
+    private String address;
+
+    @Column
+    private String location;
+
+    @Column
+    private String country;
+
+    @Column
+    private Date created_at;
+
+    @Column
+    private Date updated_at;
+
+   // @OneToMany(mappedBy = "users")
+  //  private List<account> account = new ArrayList();
+
+
 
 
 }
