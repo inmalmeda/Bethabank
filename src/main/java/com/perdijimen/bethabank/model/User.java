9e711638f5f15aedc3ba371ea283b3f4d8097827
mperdiguero <perdi666@gmail.com>
2021-05-11 17:05:21 +0200

    Update attributes constructor getters and setters user

--- src/main/java/com/perdijimen/bethabank/model/User.java
+++ src/main/java/com/perdijimen/bethabank/model/User.java
@@ -51,6 +51,144 @@ public class User {
   //  private List<account> account = new ArrayList();
 
 
+    public User() {
+    }
 
+    public User(String name, String lastname, Date birth_day, String phone, String email, String password, String DNI, String address, String location, String country, Date created_at, Date updated_at) {
+        this.name = name;
+        this.lastname = lastname;
+        this.birth_day = birth_day;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.DNI = DNI;
+        this.address = address;
+        this.location = location;
+        this.country = country;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getBirth_day() {
+        return birth_day;
+    }
+
+    public void setBirth_day(Date birth_day) {
+        this.birth_day = birth_day;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", birth_day=" + birth_day +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", DNI='" + DNI + '\'' +
+                ", address='" + address + '\'' +
+                ", location='" + location + '\'' +
+                ", country='" + country + '\'' +
+                ", created_at=" + created_at +
+                ", updated_at=" + updated_at +
+                '}';
+    }
 }
