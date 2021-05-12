de182e2bb37aca59f33fe0116e0a6f523448ca30
mperdiguero <perdi666@gmail.com>
2021-05-12 08:40:00 +0200

    add user service impl only created and updated

---
+++ src/main/java/com/perdijimen/bethabank/services/UserService.java
@@ -0,0 +1,11 @@
+package com.perdijimen.bethabank.services;
+
+import com.perdijimen.bethabank.model.User;
+
+public interface UserService {
+
+
+    User createUser(User user);
+
+    User updateUser(User user);
+}
---
+++ src/main/java/com/perdijimen/bethabank/services/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.perdijimen.bethabank.services.impl;
+
+import com.perdijimen.bethabank.model.User;
+import com.perdijimen.bethabank.repository.UserRepository;
+import com.perdijimen.bethabank.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+    private final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User createUser(User user) {
+        log.info("createUSer");
+        if (ObjectUtils.isEmpty(user))
+            return null;
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(User user) {
+        log.info("updateUser");
+        if (ObjectUtils.isEmpty(user))
+            return null;
+        return userRepository.save(user);
+    }
+}
